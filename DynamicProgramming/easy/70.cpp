940b31f6dce3cf6a683c2557a6b0b66069ce4f43
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-02-26 14:02:06 +0900

    add 70

---
+++ DynamicProgramming/easy/70.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// best approach is app 3.
+
+// approach1: naive programming (fibonacci, recursive)
+// TLE
+// class Solution
+// {
+// public:
+//     int climbStairs(int n)
+//     {
+//         if (n == 1)
+//             return 1;
+//         if (n == 2)
+//             return 2;
+//         return climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+// };
+
+// // approach2: Top Down Approach (memo recursive)
+// int t[46] = {0}; // constraints: 1 <= n <= 45
+// class Solution
+// {
+// public:
+//     int climbStairs(int n)
+//     {
+//         if (n == 1)
+//         {
+//             // t[n] = 1; // not necessary
+//             return 1;
+//         }
+//         else if (n == 2)
+//         {
+//             // t[n] = 2; // not necessary
+//             return 2;
+//         }
+//         else
+//         {
+//             if (t[n] != 0)
+//                 return t[n];
+//             else
+//             {
+//                 t[n] = climbStairs(n - 1) + climbStairs(n - 2);
+//                 return t[n];
+//             }
+//         }
+//     }
+// };
+
+// approach3: Bottom Up Approach (dynamic programming)
+// 再帰を利用せずに、最初から配列を利用。
+// 配列に計算結果を格納することで、計算の重複を防げる。
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        int t[n + 1];
+        for (int i = 0; i <= n; i++)
+            t[i] = 0;
+
+        t[1] = 1; // constraints: 1 <= n <= 45
+        if (n >= 2)
+            t[2] = 2;
+
+        for (int i = 3; i <= n; i++)
+            t[i] = t[i - 1] + t[i - 2];
+
+        return t[n];
+    }
+};
+
+int main()
+{
+    Solution sol;
+    cout << sol.climbStairs(3) << endl;
+}
